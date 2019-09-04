65dca927c958a45f1ade8f13a2d3292a4c617d15
Ân <tranthian09122001@gmail.com>
2019-09-04 10:13:38 +0700

    Create on tap 1.cpp

---
+++ on tap 1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+	int a,b,max;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	if(a>=b){
+		printf("so lon hon la: a=%d\n",a);
+	}else{
+		printf("so lon hon la b=%d\n",b);	
+	}
+	return 0;
+}

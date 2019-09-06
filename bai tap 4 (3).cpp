9ca344e8606d3e3e9ba5974778bb12c83c3ac65b
Ân <tranthian09122001@gmail.com>
2019-09-06 12:32:43 +0700

    Create bai tap 4 (3).cpp

---
+++ bai tap 4 (3).cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+	char n;
+	scanf("%c",&n);
+	
+	while(n!='T2' or n!='T3' or n!='T4' or n!='T5' or n!='T6' or n!='T7'){
+	printf("nhap lai\n");
+	scanf("%c",&n);	
+	}
+	return 0;
+}

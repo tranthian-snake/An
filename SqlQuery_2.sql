fdb2a7bd7fe6bfc2987de52ab2207488fe8ceb48
an <54616933+tranthian-snake@users.noreply.github.com>
2020-01-08 16:19:36 +0700

    Add files via upload

---
+++ SqlQuery_2.sql
@@ -0,0 +1,82 @@
+﻿INSERT INTO dbo.sp_hotdeal_beautycare
+( name_prohotdeal,gia_goc ,gia_sale ,sl_sphotdeal ,don_vi ,loai_sp)
+VALUES
+( N'Best Product To Repair Damaged Hair – Strength & Protect Promotion Pack ',75.90,59.90,100,N'bộ',N'Hair'),
+( N'Heat Protect & Rejuvenation Duo',75.90, 38.90,100,N'bộ',N'Hair'),
+
+( N'Great Skin Anywhere Set for Combination Skin',67.90,79.90,100,N'bộ',N'skin'),
+( N'3-Step Skin Care System For Skin Types 3, 4 Combination Oily to Oily Skins',38.90,55.90,100,N'bộ',N'skin'),
+
+( N'Party Time Trio Color Collection',17.90,41.90,100,N'bộ',N'make_up'),
+( N'Best Brows Ever Kit',30.00,48.90,100,N'bộ',N'make_up'),
+
+( N'Barry M Glitter Nail Polish Bundle',140.00,159.90,100,N'bộ',N'nail'),
+( N'Nails INC Sparkle Like A Unicorn Duo',15.90,20.00,100,N'bộ',N'nail'),
+
+( N'Jasmine Jewelry Set',169.90,319.90,100,N'chiếc',N'Jewelry'),
+( N'Belle Jewelry Set',166.95,285.00,100,N'chiếc',N'Jewelry'),
+
+( N'Embossed Ivory Wrap Wedding Invitation Kit By Celebrate It™',44.99,50.00,100,N'bộ',N'wedding'),
+( N'Gartner Studios® Pearl Foil Border Print at Home Invitation Kit',24.99,28.00,100,N'bộ',N'wedding');
+
+INSERT INTO dbo.san_pham_teamvits
+( name_product,gia_tien,so_luong ,don_vi ,loai)
+VALUES
+( N'Volume Shampoo & Conditioner Duo', 34.90,100,N'bộ',N'Hair'),
+( N'Best Hair Repair Products UK – Moisture & Repair Promotion Pack',78.90,100,N'bộ',N'Hair'),
+( N'Silver Shampoo & Conditioner Duo',35.90,100,N'bộ',N'Hair'),
+( N'Heat Protect & Rejuvenation Duo',47.90,100,N'bộ',N'Hair'),
+( N'Silver Shampoo & Conditioner Duo',35.90,100,N'bộ',N'Hair'),
+( N'Best Hair Products For Volume Fine Hair – Volume Promotion Pack',72.90,100,N'bộ',N'Hair'),
+( N'SDate Night Promotion Pack',51.90,100,N'bộ',N'Hair'),
+
+( N'Great Skin, Great Deal Set for Combination Oily Skin',36.90,100,N'bộ',N'skin'),
+( N'3-Step Skin Care System For Skin Type 2 Dry to Dry Combination Skin',48.90,100,N'bộ',N'skin'),
+( N'3-Step Introductory Kit For Oilier Skins',19.90,100,N'bộ',N'skin'),
+( N'Vinoperfect Natural Brightening Stars',75.90,100,N'bộ',N'skin'),
+( N'Hydration Dream Team',39.90,100,N'bộ',N'skin'),
+( N'Rose N Shine Skincare Set',85.90,100,N'bộ',N'skin'),
+( N'Moisture Miracle Set',38.90,100,N'bộ',N'skin'),
+
+( N'Party Popper Ultimate Lipstick Love Mini Kit',17.90,100,N'bộ',N'make_up'),
+( N'Merry Metals Brush Set',35.90,100,N'bộ',N'make_up'),
+( N'Prime, Set & Glow',15.90,100,N'bộ',N'make_up'),
+( N'Lash Obsessions Set',65.90,100,N'bộ',N'make_up'),
+( N'Kitten Mini: Crazy for Kittens! Mini Makeup Set',34.00,100,N'bộ',N'make_up'),
+( N'Gold Deco Eye Quad Eyeshadow Palette',75.90,100,N'bộ',N'make_up'),
+( N'Museum of Ice Cream x Sephora Collection',38.90,100,N'bộ',N'make_up'),
+
+( N'Nails INC Rolling With My Charm',19.90,100,N'bộ',N'nail'),
+( N'Nails INC Super Sexy Strong Duo',15.90,100,N'bộ',N'nail'),
+( N'NAILS INC Just Kinda Bliss Duo',15.90,100,N'bộ',N'nail'),
+( N'Nails INC The Future Is Fairy Nail Polish Duo',15.90,100,N'bộ',N'nail'),
+( N'Nails INC Mermaid Duo',15.90,100,N'bộ',N'nail'),
+( N'Nails INC Flock You Trend Duo',15.90,100,N'bộ',N'nail'),
+( N'Nails INC Seeing Stars Duo',15.90,100,N'bộ',N'nail'),
+
+( N'Ariel Pearl Charm by Pandora Jewelry',750.00,100,N'chiếc',N'Jewelry'),
+( N'Sleeping Beauty Briar Rose Earrings by RockLove',1099.90,100,N'chiếc',N'Jewelry'),
+( N'Mickey Mouse Crisscross Ring by Rebecca Hook',1259.90,100,N'chiếc',N'Jewelry'),
+( N'Frozen 2 Crystal Snowflake Ring by RockLove',759.90,100,N'chiếc',N'Jewelry'),
+( N'Toy Story To Infinity and Beyond Necklace – Women',229.90,100,N'chiếc',N'Jewelry'),
+( N'Mickey Mouse June Birthstone Necklace for Women – Light Amethyst',560.00,100,N'chiếc',N'Jewelry'),
+( N'Genie and Genie Lamp Bangles by Alex and Ani – Aladdin – Set of 2',799.90,100,N'chiếc',N'Jewelry'),
+
+( N'Little Crown Of Wishes',34.00,100,N'chiếc',N'wedding'),
+( N'Flower Girl's Dream Basket',39.99,100,N'giỏ',N'wedding'),
+( N'Gartner Studios® Sealing Wax Stamper, Fleur De Lis',7.99,100,N'bộ',N'wedding'),
+( N'Always you',90.00,100,N'bộ',N'wedding'),
+( N'Gartner Studios® Pearl Foil Swirls Print at Home Invitation Kit',24.99,100,N'bộ',N'wedding'),
+( N'Gartner Studios® Gold Glitter Invitation Kit',19.99,100,N'bộ',N'wedding'),
+( N'Cut the Cake',120.00,100,N'bộ',N'wedding'),
+
+( N'Gartner Studios® Formal Gold Foil Swirl Invitation',19.99,100,N'bộ',N'wedding'),
+( N'Gartner Studios® Foil Birds on Kraft Invitation',24.99,100,N'giỏ',N'wedding'),
+( N'White Babys Breath Bundle by Ashland®',9.99,100,N'bó',N'wedding'),
+( N'Rose & Hydrangea Bundle by Ashland®',7.99,100,N'bó',N'wedding'),
+( N'Cactus & Fern Bundle by Ashland®',13.49,100,N'bó',N'wedding'),
+( N'Swirl Silver Wedding Invitation Kit By Celebrate It™',39.99,100,N'bộ',N'wedding'),
+( N'Hot Pink Peony Bunch by Ashland®',14.99,100,N'bó',N'wedding')
+( N'Gartner Studios® Pearl Foil Swirls Print at Home Invitation Kit',24.99,100,N'bộ',N'wedding'),
+( N'Rose Gold Floral Wedding Invitation Kit By Celebrate It™',39.99,100,N'bộ',N'wedding'),
+( N'9.5" Pink Rose Bundle',26.99,100,N'bó',N'wedding');

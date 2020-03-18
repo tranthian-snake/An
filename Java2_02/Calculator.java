316ff0ccad2c1524bca057bd4cbdeede226eea4c
an <54616933+tranthian-snake@users.noreply.github.com>
2020-03-18 15:07:32 +0700

    Add files via upload

---
+++ Java2_02/Calculator.java
@@ -0,0 +1,10 @@
+package Java2_02;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Java2_02/Temprature.java
@@ -0,0 +1,30 @@
+package Java2_02;
+
+import java.util.Scanner;
+import  java.util.Arrays;
+
+public class Temprature {
+    public static void main(String[] args) {
+        double sum, average;
+        int n;
+        System.out.print("Number of days to enter temperature: ");
+        Scanner scanner = new Scanner(System.in);
+        n=scanner.nextInt();
+        int[] array = new int[n];
+        Scanner fiveinteger = new Scanner(System.in);
+        System.out.println("Enter "+n+" the temperature of n consecutive days");
+        sum=0;
+        for(int i=0;i<array.length;i++){
+                array[i]=fiveinteger.nextInt();
+                sum=sum+array[i];
+            }
+        for(int i=0;i<array.length;i++){
+            System.out.println("Element "+i+", types value was "+ array[i]);
+        }
+
+            average=sum/array.length;
+            System.out.printf("Average is: %.2f%n", average);
+
+    }
+
+ }

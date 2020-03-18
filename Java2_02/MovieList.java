77a7eb8374b4a5826f8332f868ec944f85502d33
an <54616933+tranthian-snake@users.noreply.github.com>
2020-03-18 19:22:39 +0700

    Add files via upload

--- Java2_02/MovieList.java
+++ Java2_02/MovieList.java
@@ -8,13 +8,29 @@ public class MovieList {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         List<String> movie = new ArrayList<>();
-        System.out.println("Enter the number of favorite movies");
+        System.out.print("Enter the number of favorite movies: ");
         int size;
         size=scanner.nextInt();
+        System.out.println("Enter the list of favorite movies:");
+        String[]  array = new String[size];
+        Scanner scanners = new Scanner(System.in);
         for(int i=0;i<size;i++){
-            movie=scanner.next();
+            array[i]=scanners.nextLine();
+            String movies= array[i];
+            movie.add(movies);
         }
+        System.out.println("List of favorite movies: "+ movie);
 
-        System.out.println("Enter the list of favorite movies");
+        movie.add("Blachki");
+        System.out.println("Add new movies to the list: "+ movie);
+
+        movie.set(2,"Try or hetr");
+        System.out.println("Edit existing movie name: "+ movie);
+
+        movie.remove(3);
+        System.out.println("Delete a movie from favorites: "+ movie);
+
+        System.out.println("Is the movie on the list? "+movie.contains("Math") );
+
     }
 }

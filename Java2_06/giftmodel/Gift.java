f5995a5e5c8e92f058d077702767a4a7afcd44ab
an <54616933+tranthian-snake@users.noreply.github.com>
2020-03-28 13:36:10 +0700

    Add files via upload

---
+++ Java2_06/giftmodel/Gift.java
@@ -0,0 +1,8 @@
+package Java2_06.giftmodel;
+import java.util.ArrayList;
+public class Gift {
+    private ArrayList<String> id= new ArrayList<>();
+    private ArrayList<String> name= new ArrayList<>();
+    private ArrayList<String> price= new ArrayList<>();
+    private ArrayList<String> qty= new ArrayList<>();
+}

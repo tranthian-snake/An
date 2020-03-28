31b9b84a7eb6bf7c1e1a6a25c02a390c50864b8a
an <54616933+tranthian-snake@users.noreply.github.com>
2020-03-28 13:39:50 +0700

    Add files via upload

--- Java2_06/giftmodel/Gift.java
+++ Java2_06/giftmodel/Gift.java
@@ -1,8 +1,8 @@
 package Java2_06.giftmodel;
 import java.util.ArrayList;
 public class Gift {
-    private ArrayList<String> id= new ArrayList<>();
-    private ArrayList<String> name= new ArrayList<>();
-    private ArrayList<String> price= new ArrayList<>();
-    private ArrayList<String> qty= new ArrayList<>();
+    private int id;
+    private String name;
+    private double price;
+    private int qty;
 }

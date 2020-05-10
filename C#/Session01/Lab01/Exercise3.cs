feba4f94c491671cc2bd5f398bd5edc16ee2f611
an <54616933+tranthian-snake@users.noreply.github.com>
2020-05-10 18:19:14 +0700

    Add files via upload

--- C#/Session01/Lab01/Exercise3.cs
+++ C#/Session01/Lab01/Exercise3.cs
@@ -1,12 +1,39 @@
 ﻿using System;
-
-namespace Exercise3
-{
     class Program
     {
-        static void Main(string[] args)
+    static void Main(string[] args)
+    {
+        int number;
+        do
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter n(between 1 and 7): ");
+            number = Convert.ToInt32(Console.ReadLine());
+        } while (number < 1 || number > 7);
+        switch (number) {
+
+            case 1:
+                Console.WriteLine("Monday");
+                break;
+            case 2:
+                Console.WriteLine("Tuesday");
+                break;
+            case 3:
+                Console.WriteLine("Wednesday");
+                break;
+            case 4:
+                Console.WriteLine("Thursday");
+                break;
+            case 5:
+                Console.WriteLine("Friday");
+                break;
+            case 6:
+                Console.WriteLine("Saturday");
+                break;
+            case 7:
+                Console.WriteLine("Sunday");
+                break;
+
         }
+        Console.ReadLine();
     }
-}
+    }

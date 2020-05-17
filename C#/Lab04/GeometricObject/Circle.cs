a2ca0f6ae4f7b8c200205d1ea88ca08aefb5bc09
an <54616933+tranthian-snake@users.noreply.github.com>
2020-05-17 17:11:39 +0700

    Add files via upload

---
+++ C#/Lab04/GeometricObject/Circle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+//Circle class extends GeometricObject class
+public class Circle : GeometricObject
+{
+    private double radius;
+    public Circle(double x)
+    {
+        this.radius = x;
+    }
+    public Circle(double x, string c, double w) : base(c, w)
+    {
+        this.radius = x;
+    }
+    public override String ToString()
+    {
+        return "Circle has: radius is " + radius + ", color is " + PColor + ",weight is "  + PWeight;
+    }
+    public override double findArea() { return Math.PI * radius * radius; }
+    public override double findPerimeter() { return 2 * Math.PI * radius; }
+}

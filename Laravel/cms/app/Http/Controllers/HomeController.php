eb685b9d6204c5638e815ccc9f8dd4457dac1950
an <54616933+tranthian-snake@users.noreply.github.com>
2020-07-04 19:43:35 +0700

    Add files via upload

---
+++ Laravel/cms/app/Http/Controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class HomeController extends BaseController
+{
+ public function showWelcome(){
+//     return 'Home Controller DANG KIM THI';
+//     return view('hello');
+     return view('index');
+ }
+}
